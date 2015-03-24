06312e572c4feac1aa14b456f872943f919e1338
Lorenzo Murrocu <lorenzo.murrocu@gmail.com>
2015-03-24 23:32:35 +0100

    basic bugs respawn and movement

--- js/app.js
+++ js/app.js
@@ -6,8 +6,9 @@ var Enemy = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
-    this.x = 83*-1;
-    this.y = 83*3;
+    this.x = 101*-1;
+    this.y = 83*Math.floor((Math.random() * 3) + 1);
+    this.speed = Math.floor((Math.random() * 70) + 10);
 }
 
 // Update the enemy's position, required method for game
@@ -16,7 +17,10 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-    this.x = this.x + 30 * dt;
+    this.x = this.x + this.speed * dt;
+    if(this.x > 101 * 5){
+        this.x = 101*-1;
+    }
 
 }
 
@@ -70,7 +74,7 @@ Player.prototype.render = function(){
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-var allEnemies = [new Enemy(), new Enemy(), new Enemy()];
+var allEnemies = [new Enemy(), new Enemy(), new Enemy(), new Enemy(), new Enemy(), new Enemy()];
 var player = new Player();
 
 // This listens for key presses and sends the keys to your
